769c486e7a47f48a990318c89310843cc02bc732
anson40512 <anson40512@gmail.ccom>
2023-10-06 17:46:24 +0800

    update tb.v

--- src/tb.v
+++ src/tb.v
@@ -23,20 +23,19 @@ module tb ();
     reg  [7:0] ui_in;
     reg  [7:0] uio_in;
 
-    wire [6:0] segments = uo_out[6:0];
     wire [7:0] uo_out;
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_2bits_alu dut(
-    .ui_in      (ui_in),    // Dedicated inputs
-    .uo_out     (uo_out),   // Dedicated outputs
-    .uio_in     (uio_in),   // IOs: Input path
-    .uio_out    (uio_out),  // IOs: Output path
-    .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-    .ena        (ena),      // enable - goes high when design is selected
-    .clk        (clk),      // clock
-    .rst_n      (rst_n)     // not reset
+    tt_um_4bits_alu_an dut(
+      .ui_in      (ui_in),    // Dedicated inputs
+      .uo_out     (uo_out),   // Dedicated outputs
+      .uio_in     (uio_in),   // IOs: Input path
+      .uio_out    (uio_out),  // IOs: Output path
+      .uio_oe     (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+      .ena        (ena),      // enable - goes high when design is selected
+      .clk        (clk),      // clock
+      .rst_n      (rst_n)     // reset_n - low to reset
   );
 
 endmodule
